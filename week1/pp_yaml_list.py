855a47b3bf84834beb8bbee4387a299af024069d
pseudonode <andre.costa.azevedo@gmail.com>
2018-01-19 00:45:14 -0800

    Commiting for the first time!

---
+++ week1/pp_yaml_list.py
@@ -0,0 +1,10 @@
+filename = "list.yml"
+
+import yaml
+from pprint import pprint
+
+def read_yaml(filename):
+    with open(filename) as f:
+        return yaml.load(f)
+
+pprint(read_yaml(filename))
